429d08b4a201b1a6fc007806dc5455c41f70768b
Naveen <naveenramavath133@gmail.com>
2026-07-04 09:22:27 +0530

    initial commit for eureka server

---
+++ src/test/java/com/naveen/bank/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.bank.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
